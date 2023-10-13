bbc4538fe5568a05817eefe6487ac418d98e0d78
bodie1616 <120243516+bodie1616@users.noreply.github.com>
2023-10-12 20:58:57 -0700

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,212 @@
+import time
+import os
+print("Welcome To Bank Of America CLI")
+var = input(">> ")
+if var == "execute from Code.org/code/do":
+    print("000100010010010100101010")
+    time.sleep(0.1)
+    print("11001001001010100101010101....")
+    time.sleep(0.2)
+    print("...")
+    time.sleep(2)
+    print("Loading...")
+    time.sleep(3)
+    print("000100010010010100101010")
+    time.sleep(0.1)
+    print("11001001001010100101010101....")
+    time.sleep(0.1)
+    print("000100010010010100101010")
+    time.sleep(0.1)
+    print("11001001001010100101010101....")
+    time.sleep(0.2)
+    print("000100010010010100101010")
+    time.sleep(0.1)
+    print("11001001001010100101010101....")
+    time.sleep(0.2)
+    print("000100010010010100101010")
+    time.sleep(0.1)
+    print("11001001001010100101010101....")
+    time.sleep(0.2)
+    print("000100010010010100101010")
+    time.sleep(0.1)
+    print("11001001001010100101010101....")
+    time.sleep(0.2)
+    print("intalizing...")
+    time.sleep(0.0001)
+    print("Installing dependencies...")
+    time.sleep(0.4)
+
+    print("Dependencies installed successfully!")
+    time.sleep(0.0001)
+
+    print("Running the program...")
+    time.sleep(0.0001)
+# More fancy animations or text
+    print("Program executed successfully!")
+    time.sleep(3)
+    var2 = input(">> ")
+    if var2 == "nasm Code.org/file.asm":
+        print("Compiling...")
+        time.sleep(5)
+        print("Compiled Successfully!")
+        var3 = input(">> ")
+        if var3 == "qemu_access database --txt":
+            print("Accessing...")
+            time.sleep(2)
+            print("Converting...")
+            time.sleep(3)
+            print("Converted!")
+            time.sleep(2)
+            print("Displaying Contents...")
+            time.sleep(2)
+            print("Done! Displaying...")
+            time.sleep(1)
+            print("Password: 8721")
+            print(" ")
+            print("You need a password to continue.")
+            var4 = input("Password: ")
+            if var4 == "8721":
+                print("Correct!")
+                time.sleep(2)
+                print("We still want to verify that you are a member.")
+                time.sleep(0.1)
+                print("Sending Access...")
+                time.sleep(2)
+                print("Unauthorized...")
+                var5 = input(">> ")
+                if var5 == "force_access":
+                    print("Forcing...")
+                    time.sleep(3)
+                    print("Done!")
+                    time.sleep(4)
+                    print("Access Granted!")
+                    print(" ")
+                    print("Hello! We don't think we know your name quite yet! Please Enter name to continue.")
+                    name = input(">> ")
+                    print("Hello " + name + "! Good to know that I can call you " + name + "!")
+                    print(" ")
+                    time.sleep(2)
+                    print("Hello " + name + "! So, Do what do you want to do?")
+                    print("Type 1 to deposit money to an account")
+                    deposit = input(">> ")
+                    if deposit == "1":
+                        money = input("How Much? > ")
+                        print("We will deposit $" + money)
+                        time.sleep(3)
+                        print("Deposited!")
+                        time.sleep(5)
+                        os.system("cls")
+                        print("Charlie: Good! now deposit half of that money to crime.net and finally you get the other half. Don't be greedy.")
+                        var6 = input(">> ")
+                        if var6 == "crime.net" or "Crime.net":
+                            os.system("cls")
+                            print("Welcome to Crime.net!")
+                            print("Enter a command.")
+                            com = input(">> ")
+                            if com == "deposit --half":
+                                print("Depositing...")
+                                time.sleep(2)
+                                print("Deposited!")
+                                print("Thank you for depositing! Where would you like the rest")
+                                loc = input(">> ")
+                                if loc == "Parent.me" or "parent.me":
+                                    print("Depositing other half to you...")
+                                    time.sleep(2)
+                                    print("Deposited!")
+                                    print("Total Money Left: $0")
+                                    time.sleep(3)
+                                    os.system("cls")
+                                    print("Charlie: Good Job. Thank you for not being greedy.")
+                                    print("         Now go to Ziscord for a new mission")
+                                    go = input(">> ")
+                                    if go == "ziscord" or "Ziscord":
+                                        print("1 New Message by Charlie")
+                                        print("Type 1 to view")
+                                        see = input(">> ")
+                                        if see == "1":
+                                            print("Charlie: Hello! You can take a break")
+                                            time.sleep(3)
+                                            os.system("cls")
+                                            print("Going to sleep...")
+                                            time.sleep(3)
+                                            os.system("cls")
+                                            print("Day 2")
+                                            print("---------------")
+                                            print("Go on Ziscord.")
+                                            var7 = input(">> ")
+                                            if var7 == "ziscord" or "Ziscord":
+                                                print("1 New Message From Charlie")
+                                                print("Type 1 to view")
+                                                var8 = input(">> ")
+                                                if var8 == "1":
+                                                    print("Charlie: Hey, here is the plan for mission 2")
+                                                    print("         I need you to get this guy's ip")
+                                                    print("         His name is Jame Racs")
+                                                    var9 = input(">> ")
+                                                    if var9 == "cmd" or "CMD":
+                                                        os.system("cls")
+                                                        print("""
+Microsoft Windows [Version 10.0.19045.3516]
+(c) Microsoft Corporation. All rights reserved.
+                                                              
+                                                              """)
+                                                        var10 = input("C:\\Users\\Unknown>")
+                                                        if var10 == "nmap james racs" or "nmap James Racs" or "nmap James racs" or "nmap james Racs":
+                                                            print("Locating System...")
+                                                            time.sleep(3)
+                                                            print("Accessing Files...")
+                                                            time.sleep(2)
+                                                            print("Done!")
+                                                            time.sleep(0.3)
+                                                            print("""
+Documents/
+Pictures/
+info/
+Users/
+                                                                  """)
+                                                            var11 = input(">> ")
+                                                            if var11 == "ls info":
+                                                                print("""
+ip.txt
+                                                                      """)
+                                                                var12 = input(">> ")
+                                                                if var12 == "cat ip.txt":
+                                                                    os.system("cls")
+                                                                    print("217.157.227.208")
+                                                                    var13 = input(">> ")
+                                                                    if var13 == "copy ip.txt":
+                                                                        print("Copied!")
+                                                                        var14 = input(">> ")
+                                                                        if var14 == "ziscord" or "Ziscord":
+                                                                            
+                                                                            print("Charlie: Hey, here is the plan for mission 2")
+                                                                            print("         I need you to get this guy's ip")
+                                                                            print("         His name is Jame Racs")
+                                                                            print(" ")
+                                                                            print(" ")
+                                                                            print(" ")
+                                                                            print(" ")
+                                                                            print(" ")
+                                                                            print(" ")
+                                                                            time.sleep(3)
+                                                                            print("You: Ip: 217.157.227.208")
+                                                                            time.sleep(3)
+                                                                            print("Charlie: Thanks! go to crime.net to get your money  ")
+                                                                            var15 = input(">> ")
+                                                                            if var15 == "crime.net" or "Crime.net":
+                                                                                os.system("cls")
+                                                                                print("Welcome to Crime.net")
+                                                                                print("Enter a command.")
+                                                                                var16 = input(">> ")
+                                                                                if var16 == "get pay":
+                                                                                    print("Getting Payment...")
+                                                                                    time.sleep(3)
+                                                                                    print("Done!")
+                                                                                    time.sleep(5)
+                                                                                    print("Going to sleep...")
+                                                                                    time.sleep(3)
+                                                                    
+                                                                
+                                
+
+    
